8c70dd8356c3e3df344849b5e43b951911fb5b25
Satoshi Shiraishi <sachoco@gmail.com>
2015-01-16 13:18:54 +0100

    debug

--- header.php
+++ header.php
@@ -52,7 +52,19 @@
         <section id="intro" class="snap">
             <div class="overlay"></div>
             <div class="content">
-                
+                <div class="profile">
+                    <div>
+                        <div class="logo">
+                            <img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" />
+                        </div>
+                        <div class="roles">バッハの音楽を未来へ</div>
+
+                        <div class="short-profile">
+                            J.S.バッハの音楽は人類共通の財産です<br>私たちバッハ・コレギウム・ジャパンは創設以来、日本から世界へバッハ音楽を発信し続けてきました。<br>この素晴らしい音楽を一人でも多くの人と共有したい<br>それが私たちの願いです。
+                        </div>        
+                    </div>
+                </div>
+
             </div>
         </section>
         <!-- Intro END -->
